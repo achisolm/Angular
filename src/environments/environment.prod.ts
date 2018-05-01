f5c1c2866e38ba07d2f6663f28aa70c9959bbc8b
Adam Chisolm <axchisolm@gmail.com>
2018-04-30 22:20:25 -0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmR0QaDKZt5HmbH8JrZjDFXeI8rf090Ug",
+    authDomain: "fir-chat-d5bb1.firebaseapp.com",
+    databaseURL: "https://fir-chat-d5bb1.firebaseio.com",
+    projectId: "fir-chat-d5bb1",
+    storageBucket: "fir-chat-d5bb1.appspot.com",
+    messagingSenderId: "852201090705"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmR0QaDKZt5HmbH8JrZjDFXeI8rf090Ug",
+    authDomain: "fir-chat-d5bb1.firebaseapp.com",
+    databaseURL: "https://fir-chat-d5bb1.firebaseio.com",
+    projectId: "fir-chat-d5bb1",
+    storageBucket: "fir-chat-d5bb1.appspot.com",
+    messagingSenderId: "852201090705"
+  }
 };
